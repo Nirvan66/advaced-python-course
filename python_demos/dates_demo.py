d19b0f2b1e5e3975f3106f3902e5a9c946bb68df
nir11152 <ntheethira@esri.com>
2022-03-21 13:11:13 -0700

    first commit

---
+++ python_demos/dates_demo.py
@@ -0,0 +1,9 @@
+from datetime import date, datetime, timedelta
+import time
+
+def run_demo() -> None:
+    current_time = time.time()
+    print(current_time)
+
+if __name__=="__main__":
+    run_demo()
